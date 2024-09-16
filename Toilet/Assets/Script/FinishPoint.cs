747db68613c10023e24e0a40d72672d6edf36015
Thanaphat_T <jjmanzero@gmail.com>
2024-09-16 21:35:30 +0700

    New Level Ending System Add

--- Toilet/Assets/Script/FinishPoint.cs
+++ Toilet/Assets/Script/FinishPoint.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishPoint : MonoBehaviour
 {
-    [SerializeField] private bool goNextLevel;
-    [SerializeField] private string levelName;
-    //public GameObject gameOverUI;
+    public GameObject gameOverUI;
+    public PlayerCamera pc;
+    public PlayerScript ps;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
-            
-            if (goNextLevel)
-           {
-                SceneController.instance.NextLevel();
-            }
-            else
-            {
-                SceneController.instance.LoadScene(levelName);
-            }
+            pc.GetComponent<PlayerCamera>().enabled = false;
+            ps.GetComponent<PlayerScript>().enabled = false;
+            GameOver();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
-    
-   // private void Gameover()
-   // {
-   //     gameOverUI.SetActive(true);
-   // }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    public void SelectLevel(string String)
+    {
+        SceneController.instance.LoadScene(String);
+    }
+
+    public void NextLevel()
+    {
+        SceneController.instance.NextLevel();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
 
 }
